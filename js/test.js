072e9b5e1402983e64bdbeafc98bc8153bee860f
yimoxiamu <2959623954@qq.com>
2018-06-26 13:33:32 +0800

    更换板式

--- js/test.js
+++ js/test.js
@@ -1,12 +1,13 @@
 console.info("进入axios");
-// var instance = axios.create({
-//     headers: {'content-type': 'application/x-www-form-urlencoded'}
-// });
-// instance .post("/test", Qs.stringify({"name":"name"})).then(function (value) { console.info(value) });
+var instance = axios.create({
+    headers: {'content-type': 'application/x-www-form-urlencoded'}
+});
+instance .post("/type/test", Qs.stringify({name:"name",age:12})).then(function (value) { console.info(value.data) });
+
+
 
 //获取默认blogtype
 axios.get("/type/defaultType").then(function (data) {
-    console.info(data.data.retMsg[0].id);
     if (data.data.retCode==0){
         new Vue({
             el:'#blog_type',
